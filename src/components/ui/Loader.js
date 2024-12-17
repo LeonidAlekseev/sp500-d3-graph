d25756afc55a1e144ea3479493b1af68971d481a
LeonidAlekseev <leonidalekseevv@mail.ru>
2024-12-18 01:42:03 +0300

    Update loader

--- src/components/ui/Loader.js
+++ src/components/ui/Loader.js
@@ -1,10 +1,10 @@
 export default function Spinner() {
   return (
-    <div className="min-h-60 flex flex-col bg-white border shadow-sm rounded-xl">
+    <div className="min-h-60 flex flex-col rounded-xl">
       <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
         <div className="flex justify-center">
           <div
-            className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
+            className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-gray-600 rounded-full"
             role="status"
             aria-label="loading"
           >
